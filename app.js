0ae19f415d9ed3e074ca194e8fba5eac0354c7b8
MauroSezella <maurosezella@gmail.com>
2023-11-08 22:29:38 -0300

    Agregamos ruta para registro

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.listen(3030, () => console.log('Servidor Corriendo'));
 app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.resolve("./views/home.html")));
+
+app.get('/register',(req,res) => res.sendFile(path.resolve("./views/register.html")));

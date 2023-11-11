28aaa69b916c1db69fe03c49d9d1ee88ab532d23
MauroSezella <maurosezella@gmail.com>
2023-11-11 17:58:57 -0300

    Estilizamos el form del registro y creamos el formulario para el login con estilos

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ app.use(express.static('public'));
 app.get('/',(req,res) => res.sendFile(path.resolve("./views/home.html")));
 
 app.get('/register',(req,res) => res.sendFile(path.resolve("./views/register.html")));
+
+app.get('/login',(req,res) => res.sendFile(path.resolve("./views/login.html")));
